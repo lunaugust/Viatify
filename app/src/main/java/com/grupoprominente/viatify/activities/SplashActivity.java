1b2f63109c65ea03b37605460c5164918338e2f6
Augusto Luna <lunaugust@gmail.com>
2018-07-19 00:58:13 -0300

    Initial commit

---
+++ app/src/main/java/com/grupoprominente/viatify/activities/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.grupoprominente.viatify.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+    }
+}
